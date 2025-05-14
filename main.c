355e13793f060e309468ecb2b460ce0e00b73fc0
josh_riang <sandinya1sampai8@gmail.com>
2025-05-14 22:36:40 +0700

    Membangun kerangka utama dari kalkulator

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
 int main() {
+    int choice, angka1, angka2;
+    printf("Selamat datang di kalkulator sederhana\nSilahkan masukkan pilihan \n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian");
+    scanf("%d", &choice);
+
+    printf("Masukkan angka pertama: ");
+    scanf("%d", &angka1);
+    printf("Masukkan angka kedua: ");
+    scanf("%d", &angka2);
+
+    switch (choice) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            printf("Pilihan tidak valid.\n");
+    }
+
     return 0;
 }
