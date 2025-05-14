d95fe6db74a6fa9c5a2efbe1b8ab68c131c853e9
CokiGakDisin1 <rbatara06@gmail.com>
2025-05-14 23:02:55 +0700

    Revisi 2 Addition

--- main.c
+++ main.c
@@ -4,22 +4,6 @@ int penjumlahan(int a, int b) {
     return a + b;
 }
 
-int pengurangan(int a, int b) {
-    return a - b;
-}
-
-int perkalian(int a, int b) {
-    return a * b;
-}
-
-float pembagian(int a, int b) {
-    if (b == 0) {
-        printf("Error: Pembagian dengan nol tidak diperbolehkan.\n");
-        return 0;
-    }
-    return (float)a / b;
-}
-
 int main() {
     int choice, angka1, angka2;
 
@@ -38,13 +22,10 @@ int main() {
             printf("Hasil penjumlahan: %d + %d = %d\n", angka1, angka2, penjumlahan(angka1, angka2));
             break;
         case 2:
-            printf("Hasil pengurangan: %d - %d = %d\n", angka1, angka2, pengurangan(angka1, angka2));
             break;
         case 3:
-            printf("Hasil perkalian: %d * %d = %d\n", angka1, angka2, perkalian(angka1, angka2));
             break;
         case 4:
-            printf("Hasil pembagian: %d / %d = %.2f\n", angka1, angka2, pembagian(angka1, angka2));
             break;
         default:
             printf("Pilihan tidak valid.\n");
